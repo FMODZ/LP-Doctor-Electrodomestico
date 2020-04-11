3541cfadeed06865ff4460ed0ae204d96a6dd662
Fili Salsa <salazar.sanchez.16047@itsmante.edu.mx>
2020-04-11 18:21:56 -0500

    Nav Mobile agregado

--- js/scripts.js
+++ js/scripts.js
@@ -14,10 +14,26 @@ $(document).ready(function(){
         /*Funciones */
      function toggle(){
         var array = ['#scrollhome', '#scrollservices', '#scrollchoose', '#scrolltestimonials', '#scrollcontact'];
+        var arrayMobile = ['#mobilehome', '#mobileservices', '#mobilechoose', '#mobiletestimonials', '#mobilecontact'];
+
         $.each(array, (index, value) => {
             $(value).removeClass("fm-scroll-active");
         });
+
+        $.each(arrayMobile, (index, value) => {
+            $(value).removeClass("active");
+        });
     }
+
+    $("#nav").on("click", function(){
+        $(".mobile").toggleClass("nav-mobile");
+    })
+
+    $(".toTop").on('click', function(){
+        $("html, body").animate({scrollTop: 0}, 100);
+    });
+        
+    
 
     $(window).scroll(function(){
 
@@ -47,6 +63,12 @@ $(document).ready(function(){
 
         /*Scroll Nav */
 
+        if($(this).scrollTop() > 58){
+            $(".fm-nav-scroll").css("display", "block");
+        }else{
+            $(".fm-nav-scroll").css("display", "none");
+        }
+
         if($(this).scrollTop()>=138){
             $('.fm-nav-scroll').css("opacity","1");
             $('.fm-float-button').css("bottom","40px");
@@ -58,26 +80,31 @@ $(document).ready(function(){
         if($(this).scrollTop() < services){
             toggle();
             $("#scrollhome").addClass("fm-scroll-active");
+            $("#mobilehome").addClass("active");
         }
 
         if($(this).scrollTop() >= services && $(this).scrollTop() <= choose){
             toggle();
             $("#scrollservices").addClass("fm-scroll-active");
+            $("#mobileservices").addClass("active");
         }
 
          if($(this).scrollTop() >= choose && $(this).scrollTop() <= testimonials){
             toggle();
             $("#scrollchoose").addClass("fm-scroll-active");
+            $("#mobilechoose").addClass("active");
          }
 
          if($(this).scrollTop() >= testimonials && $(this).scrollTop() <= contact){
             toggle();
             $("#scrolltestimonials").addClass("fm-scroll-active");
+            $("#mobiletestimonials").addClass("active");
          }
 
          if($(this).scrollTop() >= contact){
             toggle();
             $("#scrollcontact").addClass("fm-scroll-active");
+            $("#mobilecontact").addClass("active");
          }
     });
 
@@ -88,6 +115,8 @@ $(document).ready(function(){
         var viewportBottom = viewportTop + $(window).height();
         return elementBottom > viewportTop && elementTop < viewportBottom;
     };
+
+    
 
     /*Estilos iniciales */
     $(".fm-image-main").css("opacity","1");
