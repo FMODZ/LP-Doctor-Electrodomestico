7fd4965940a220da07c70f76bcec896ab8f97c10
Fili Salsa <salazar.sanchez.16047@itsmante.edu.mx>
2020-04-11 15:18:33 -0500

    Se agregaron scripts y diseños

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,40 @@
 $(document).ready(function(){
-    var services = $("#services").offset().top;
-    var choose = $("#choose").offset().top;
-    var testimonials = $("#testimonials").offset().top;
-    var contact = $("#contact").offset().top;
+
+     /*Funciones */
+     function toggle(){
+        var array = ['#scrollhome', '#scrollservices', '#scrollchoose', '#scrolltestimonials', '#scrollcontact'];
+        $.each(array, (index, value) => {
+            $(value).removeClass("fm-scroll-active");
+        });
+    }
 
     $(window).scroll(function(){
+
+        /*ViewPort*/
+        if($("#servicesrow").isInViewport()){
+            $("#servicesrow").css("opacity", "1");
+        }
+
+        if($("#chooserow").isInViewport()){
+            $("#chooserow").css("opacity", "1");
+            $(".fade-choose-left").css("left", "0");
+            $(".fade-choose-right").css("right", "0");
+        }
+
+        if($("#services").isInViewport()){
+            $(".fm-subtitle-top").css({
+                "opacity": "1",
+                "bottom": "0"
+            });
+        }
+
+        
+
+        if($(".fm-subtitle-why").isInViewport()){
+            $(".fm-subtitle-why").css("opacity", "1");
+        }
+
+        /*Scroll Nav */
 
         if($(this).scrollTop()>=138){
             $('.fm-nav-scroll').css("opacity","1");
@@ -40,10 +70,38 @@ $(document).ready(function(){
          }
     });
 
-    function toggle(){
-        var array = ['#scrollhome', '#scrollservices', '#scrollchoose', '#scrolltestimonials', '#scrollcontact'];
-        $.each(array, (index, value) => {
-            $(value).removeClass("fm-scroll-active");
+    $.fn.isInViewport = function() {
+        var elementTop = $(this).offset().top;
+        var elementBottom = elementTop + $(this).outerHeight();
+        var viewportTop = $(window).scrollTop();
+        var viewportBottom = viewportTop + $(window).height();
+        return elementBottom > viewportTop && elementTop < viewportBottom;
+    };
+
+    /*Estilos iniciales */
+    $(".fm-image-main").css("opacity","1");
+    $(".fade-left").css("left","0");
+    $(".fade-right").css("right","0");
+
+    if($("#services").isInViewport()){
+        $(".fm-subtitle-top").css({
+            "opacity": "1",
+            "bottom": "0"
         });
     }
+
+    /*Hover*/
+    $(".fm-card-description").hover(function() {
+        $(this).children().first().addClass("fm-rotate");
+    });
+
+    $('.fm-card-top').bind('webkitAnimationEnd mozAnimationEnd animationEnd', function() {
+        $(this).removeClass('fm-rotate');
+      });
+
+    /*Variables */
+    var services = $("#services").offset().top;
+    var choose = $("#choose").offset().top;
+    var testimonials = $("#testimonials").offset().top;
+    var contact = $("#contact").offset().top;
 });
